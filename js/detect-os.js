b07e2d14702d2e629d8c2c5a3be6a2ac7b6dc73a
parker <parkerschellenberg@gmail.com>
2023-02-23 10:51:20 -0600

    Fix site-logo for firefox and chrome on Windows OS
    The divs that make up the site logo had very large content sizes when
    viewed on Windows OS with font Bungee. Worked fine on Mac and Linux.
    Gotta love Windows.

---
+++ js/detect-os.js
@@ -0,0 +1,9 @@
+let OSName = "Unknown-OS";
+if (navigator.appVersion.indexOf("Win")!=-1) OSName= "Windows";
+if (navigator.appVersion.indexOf("Mac")!=-1) OSName= "MacOS";
+if (navigator.appVersion.indexOf("X11")!=-1) OSName= "UNIX";
+if (navigator.appVersion.indexOf("Linux")!=-1) OSName= "Linux";
+
+// console.log(OSName);
+
+document.documentElement.classList.add(OSName)
